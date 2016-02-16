af9153991be0abae8ebfdfaa130b060d38fc63e4
ned rihine <ned.rihine@gmail.com>
2016-02-16 23:05:02 +0900

    fix update-subtree.scm

--- update-subtree.scm
+++ update-subtree.scm
@@ -4,8 +4,8 @@
 
 (use-modules (srfi srfi-13))
 
-(define %subtrees '(("./ext/io-path"       . "https://github.com:scheme-gasket/scm-io-path.git")
-                    ("./ext/shell-command" . "https://github.com:scheme-gasket/scm-shell-command.git")
+(define %subtrees '(("./ext/io-path"       . "https://github.com/scheme-gasket/scm-io-path.git")
+                    ("./ext/shell-command" . "https://github.com/scheme-gasket/scm-shell-command.git")
                     ("./ext/version"       . "https://github.com/scheme-gasket/scm-version.git")))
 
 (define (update-one pair)
